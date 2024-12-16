1ebf150dc1bfa6596d56d142c45017603c116fed
Prakash Dey <prakashdr213@gmail.com>
2024-12-16 12:50:40 +0530

    feat: upgrade and clean up old install files in ubuntu. - tested in ubnutu 24

---
+++ ubuntu/README.md
@@ -0,0 +1,4 @@
+Usage:
+
+wget -O - https://raw.githubusercontent.com/prakashdey/utilities/refs/heads/main/ubuntu/cleanup.sh | bash
+
--- ubuntu/cleanup.sh
+++ ubuntu/cleanup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get update ; 
 sudo apt-get upgrade -y;
 sudo apt autoremove && sudo apt remove $(dpkg-query --show 'linux-modules-*' | cut -f1 | grep -v "$(uname -r)")
